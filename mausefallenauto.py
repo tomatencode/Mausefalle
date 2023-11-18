c91ab3cde0ef25a64f39f62372dea87d66be1bac
Simon Thomas <ingo_thomas@gmx.de>
2023-11-18 15:54:29 +0100

    update funktion

--- mausefallenauto.py
+++ mausefallenauto.py
@@ -1,8 +1,12 @@
 from math import *
 
-u = 1 #[-] Übersetzung des getriebes
+u = 10 #[-] Übersetzung des getriebes
 rr = 1 #[m] radius des antriebsreifen
 rh = 1 #[m] länge des hebels
+
+v = 0 #[m/s] startgeschwindigkeit des autos
+m = 0.1 #[kg] masse des autos
+x = 0 #[m] startposition des autos
 
 def Fr(v):
     """
@@ -14,7 +18,10 @@ def Ff(phi):
     """
     Federkraft in abhängigkeit zum Winkel in [N]
     """
-    return 2.5 #Schätzwert
+    if phi < 180:
+        return 2.5 #Schätzwert
+    else:
+        return 0
 
 def find_phi(x,rr,u):
     """
@@ -24,7 +31,7 @@ def find_phi(x,rr,u):
     phi = x/(umfang*u)*360
     return phi
 
-def Fa(rr,rh,u,phi):
+def find_Fa(rr,rh,u,phi):
     """
     findet den impuls des autos in [N]
     """
@@ -33,5 +40,17 @@ def Fa(rr,rh,u,phi):
     Fa = Me/rr
     return Fa
 
+def update(x,v,m,rr,rh,u,dt):
+    phi = find_phi(x,rr,u)
+    Fa = find_Fa(rr,rh,u,phi)
+    dI_dt = Fa-Fr(v)
+    v = dI_dt/m
+    x += v*dt
+    return x, v
 
-print(find_phi(3.1,0.5,u))
+while find_phi(x,rr,u) < 180 or v < 0.1:
+    x_new,v_new = update(x,v,m,rr,rh,u,1/120)
+    x = x_new
+    v = v_new
+
+print(x)
