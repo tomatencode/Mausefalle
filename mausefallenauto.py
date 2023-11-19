d61a9310d0b923558d9819ad7cf0b571867baf21
tomatencode <ingo_thomas@gmx.de>
2023-11-19 20:34:39 +0100

    Mehr Kommentare

--- mausefallenauto.py
+++ mausefallenauto.py
@@ -3,55 +3,82 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy
 
-u = 1 #[-] Übersetzung des getriebes
-rr = 1 #[m] radius des antriebsreifen
-rh = 1 #[m] länge des hebels
+ü = 10 #[-] Übersetzung des getriebes
+rr = 0.1 #[m] radius des antriebsreifen
+rh = 0.1 #[m] länge des hebels
 m = 0.1 #[kg] masse des autos
 
 max_lenght = 30 #[s] maximum simulation length
 
 def Fr(v):
     """
-    Reibungswidersandkraft des Autos in ahängigkeit zur geschwindigkeit in [N]
+    Reibungswidersandkraft des Autos in ahängigkeit zur geschwindigkeit in [N] (Benutzerdefinirt)
     """
-    return v/20 #Schätzwert
+    return v / 20 # Wiederstand in [N] (Schätzwert)
 
 
 
 def Ff(phi):
     """
-    Federkraft in abhängigkeit zu den umdrehungen in [N]
+    Federkraft in abhängigkeit zu den umdrehungen in [N] (Benutzerdefinirt)
     """
+
+    # wenn die mausefalle noch nicht zu ist (phi < 0.5) gibt die falle eine konstante kraft zurück.
+    # sonst hat die falle zugeschnappt und ist nicht mehr mit der achse verbunden.
+
+    # phi[-] = anzahl der bisherigen umdrehungen
 
     if phi < 0.5:
-        return 0.02/u
+        return 2 # Kraft der Feder in [N] (Schätzwert)
     else:
-        return 0
+        return 0 # Kraft der Feder in [N]
 
-def find_phi(x,rr,u):
+def find_phi(x,rr,ü):
     """
-    findet den winkel der mausefalle in abhängigkeit zur zurückgelegten strecke in [°]
+    findet die anzahl der umdrehungen der mausefalle in abhängigkeit zur zurückgelegten strecke in [-],
     """
-    umfang = 2*pi*rr
-    phi = x/(umfang*u)
+
+    # teilt die zurückgelekte stricke durch die bisherigen umdrehungen mal das übersetzungsverhältnis.
+
+    # x[m] = zurückgelegte strecke
+    # ü[-] = überstetzungsverhältnis von Mausefalle zu Achse
+    # ur[m] = umfang des rades 
+    # rr[m] = radius des rades
+    # phi[-] = anzahl der bisherigen umdrehungen
+
+    ur = 2 * pi * rr # ur[m] = 2pi[-] * rr[m]
+    phi = x / ( ur * ü ) # phi[-] = x[m] / (ur[m] * u[-])
 
     return phi
 
-def find_Fa(rr,rh,u,phi):
+def find_Fa(rr,rh,ü,phi):
     """
-    findet den impuls des autos in [N]
+    findet die Kraft des Rades in [N]
     """
-    Ma = rh*Ff(phi)
-    Me = u*Ma
-    Fa = Me/rr
 
-    return Fa
+    # findet das Drehmoment der Mausefalle Ma
+    # benutzt das übersetztungsverhältnis um Ma in das Drehmoment der Achse Me zu überrsetzten.
+    # wandelt das drehmoment der Achse in Die kraft des Rades Fa um.
 
-def find_simulation_lenght(max_lenght,sulution,rr,u,m):
+    # Fa[N] = Kraft des Rades
+    # Ma[Nm] = Drehmoment der Mausefalle
+    # Me[Nm] = Drehmoment der Achse
+    # rh[M] = länge des hebels der Mausefalle
+    # rr[M] = Radius des Rades
+    # ü[-] = überstetzungsverhältnis von Mausefalle zu Achse
+
+    Ma = rh * Ff(phi) # Ma[Nm] = rh[m] * Ff(phi[-])[N]
+    Me = ü * Ma # Me[Nm] = ü[-] * Ma[Nm]
+    Fa = Me / rr # Fa[N] = Me[Nm] / rr[m]
+
+    return Fa # [Nm]
+
+
+def find_simulation_lenght(max_lenght,sulution,rr,ü,m,):
     for i in range(0,max_lenght):
         x = sulution.sol(i)[1]
         v = sulution.sol(i)[0]
-        if find_phi(x,rr,u) > 0.5 and v/m < 0.1:
+        if find_phi(x,rr,ü) > 0.5 and v/m < 0.1:
             return x
     return max_lenght
 
@@ -59,11 +86,11 @@ def find_simulation_lenght(max_lenght,sulution,rr,u,m):
 def f(t, y):
     [v,x] = y
     
-    return [ (find_Fa(rr,rh,u,find_phi(x,rr,u)) - Fr(v))/m, v]
+    return [ (find_Fa(rr,rh,ü,find_phi(x,rr,ü)) - Fr(v))/m, v]
 
 sulution = scipy.integrate.solve_ivp(f, [0, max_lenght], [0,0], dense_output=True)
 
-s = find_simulation_lenght(max_lenght,sulution,rr,u,m)
+s = find_simulation_lenght(max_lenght,sulution,rr,ü,m)
 s = max_lenght
 
 
